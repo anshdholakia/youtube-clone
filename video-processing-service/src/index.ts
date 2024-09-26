467cec2bcf16a0a3d19e38cd50c5ac8533c758fd
Ansh Dholakia <anshhiro.dholakia@gmail.com>
2024-09-26 16:26:30 -0400

    making express backend to process videos using ffmpeg

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
+
+const app = express();
+app.use(express.json());
+
+app.post('/process-video', (req, res)=>{
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send('Input and output file paths are required');
+    }
+
+    ffmpeg(inputFilePath).outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+
+    })
+    .on("error", (err) => {
+        console.error("Error processing video:", err);
+        res.status(500).send("Error processing video");
+    }).save(outputFilePath); // converting it to 360p
+
+    res.status(200).send('Video processed successfully');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Video processing service app listening on http://localhost:${port}`);
+})
