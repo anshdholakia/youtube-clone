a9ab83f1a828fdf3224f96334335cdd5dd026328
Ansh Dholakia <anshhiro.dholakia@gmail.com>
2024-12-13 01:09:27 -0500

    adding firebase function files

---
+++ youtube-web-client/utils/firebase/firebase.ts
@@ -0,0 +1,47 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase-admin/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, User } from "firebase/auth";
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+
+dotenv.config({ path: join(__dirname, '../../.env') });
+
+const apiKey = process.env.API_KEY;
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: "clone-1b438.firebaseapp.com",
+  projectId: "clone-1b438",
+  appId: "1:936143624285:web:77d515e069dceade2c95b4",
+  measurementId: "G-N6Q61HWB0X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+/**
+ * Signs the user in using Google Auth
+ * @returns {Promise<User>} A promise that resolves the user credentials
+ */
+export function signInWithGoogle() {
+    return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/**
+ * Signs the user out
+ */
+export function signOut() {
+    return auth.signOut();
+}
+
+/**
+ * Trigger a callback when user auth state changes
+ * @returns A function to unsubsribe callback
+ */
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+    return onAuthStateChanged(auth, callback);
+}
